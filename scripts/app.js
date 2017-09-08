414742068be772752d28fa108e41d87e48eb7335
Xiaomao <fxm@autoforce.net>
2017-09-08 16:57:55 +0800

    adding frequency visualisation

--- scripts/app.js
+++ scripts/app.js
@@ -2,9 +2,9 @@
 // that need prefixes
 
 navigator.getUserMedia = (navigator.getUserMedia ||
-    navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia ||
-    navigator.msGetUserMedia);
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia ||
+  navigator.msGetUserMedia);
 
 // set up forked web audio context, for multiple browsers
 // window. is needed otherwise Safari explodes
@@ -36,34 +36,48 @@ function makeDistortionCurve(amount) {
     deg = Math.PI / 180,
     i = 0,
     x;
-    for (; i < n_samples; i++) {
-      x = i * 2 / n_samples - 1;
-      curve[i] = (3 + k) * x * 20 * deg / (Math.PI + k * Math.abs(x));
-    }
-    console.log("curve: ", curve);
-    return curve;
+  for (; i < n_samples; i++) {
+    x = i * 2 / n_samples - 1;
+    curve[i] = (3 + k) * x * 20 * deg / (Math.PI + k * Math.abs(x));
+  }
+  console.log("curve: ", curve);
+  return curve;
 }
 
 // grab audio track via XHR for convolver node
 
+var soundSource, concertHallBuffer;
+
 ajaxRequest = new XMLHttpRequest();
-ajaxRequest.open('GET', '../audio/concert-crowd.ogg', true);
+ajaxRequest.open('GET', 'http://mdn.github.io/voice-change-o-matic/audio/concert-crowd.ogg', true);
 ajaxRequest.respondType = 'arraybuffer';
-ajaxRequest.send();
 
 var concertHallBuffer;
 
 ajaxRequest.onload = function() {
   var audioData = ajaxRequest.response;
-  soundSource = audioCtx.createBufferSource();
-  concertHallBuffer = audioCtx.createBuffer(audioData, true);
-  convolver.buffer = concertHallBuffer;
-}
 
+  audioCtx.decodeAudioData(audioData, function(buffer) {
+    concertHallBuffer = buffer;
+  }, function(e) { "Error with decoding audio data" + e.err });
+
+  soundSource = audioCtx.createBufferSource();
+  soundSource.buffer = concertHallBuffer;
+
+  // soundSource.connect(audioCtx.destination);
+  // soundSource.loop = true;
+  // soundSource.start();
+};
+
+ajaxRequest.send();
 // set up canvas context for visualizer
 
 var canvas = document.querySelector('.visualizer');
 var canvasCtx = canvas.getContext("2d");
+
+var intendedWidth = document.querySelector('.wrapper').clientWidth;
+
+canvas.setAttribute('width', intendedWidth);
 
 var visualSelect = document.getElementById("visual");
 
@@ -72,95 +86,132 @@ var drawVisual;
 //main block for doing the audio recording
 
 if (navigator.getUserMedia) {
-    console.log('getUserMedia supported.');
-    navigator.getUserMedia(
-        // constraints - only audio needed for this app
-        {
-            audio: true
-        },
+  console.log('getUserMedia supported.');
+  navigator.getUserMedia(
+    // constraints - only audio needed for this app
+    {
+      audio: true
+    },
 
-        // Success callback
-        function(stream) {
-            source = audioCtx.createMediaStreamSource(stream);
-            source.connect(analyser);
-            analyser.connect(distortion);
-            distortion.connect(biquadFilter);
-            biquadFilter.connect(convolver);
-            convolver.connect(gainNode);
-            gainNode.connect(audioCtx.destination);
+    // Success callback
+    function(stream) {
+      source = audioCtx.createMediaStreamSource(stream);
+      source.connect(analyser);
+      analyser.connect(distortion);
+      distortion.connect(biquadFilter);
+      biquadFilter.connect(convolver);
+      convolver.connect(gainNode);
+      gainNode.connect(audioCtx.destination);
 
-            visualize(stream);
-            voiceChange();
-        },
+      visualize(stream);
+      voiceChange();
+    },
 
-        // Error callback
-        function(err) {
-            console.log('The following gUM error occured: ' + err);
-        }
-    );
+    // Error callback
+    function(err) {
+      console.log('The following gUM error occured: ' + err);
+    }
+  );
 } else {
-    console.log('getUserMedia not supported on your browser!');
+  console.log('getUserMedia not supported on your browser!');
 }
 
 function visualize(stream) {
-    WIDTH = canvas.width;
-    HEIGHT = canvas.height;
+  WIDTH = canvas.width;
+  HEIGHT = canvas.height;
 
-    var visualSetting = visualSelect.value;
-    console.log(visualSetting);
+  var visualSetting = visualSelect.value;
+  console.log(visualSetting);
+  console.log("stream: ", stream);
 
-    if (visualSetting == 'sinewave') {
-        analyser.fftSize = 2048;
-        var bufferLength = analyser.frequencyBinCount;
-        var dataArray = new Uint8Array(bufferLength);
+  if (visualSetting == 'sinewave') {
+    analyser.fftSize = 2048;
+    var bufferLength = analyser.frequencyBinCount;
+    console.log("bufferLength: ", bufferLength);
+    var dataArray = new Uint8Array(bufferLength);
 
-        canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
 
-        function draw() {
-            drawVisual = requestAnimationFrame(draw);
+    function draw() {
+      drawVisual = requestAnimationFrame(draw);
 
-            analyser.getByteTimeDomainData(dataArray);
+      analyser.getByteTimeDomainData(dataArray);
 
-            canvasCtx.fillStyle = 'rgb(200, 200, 200)';
-            canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+      canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
 
-            canvasCtx.lineWidth = 2;
-            canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+      canvasCtx.lineWidth = 2;
+      canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
 
-            canvasCtx.beginPath();
+      canvasCtx.beginPath();
 
-            var sliceWidth = WIDTH * 1.0 / bufferLength;
-            var x = 0;
+      var sliceWidth = WIDTH * 1.0 / bufferLength;
+      var x = 0;
 
-            for (var i = 0; i < bufferLength; i++) {
+      for (var i = 0; i < bufferLength; i++) {
+        // console.log("dataArray[i]: ", dataArray[i]);
+        console.log("stream: ", stream);
+        var v = dataArray[i] / 128.0;
+        var y = v * HEIGHT / 2;
+        if (i === 0) {
+          canvasCtx.moveTo(x, y);
+        } else {
+          canvasCtx.lineTo(x, y);
+        }
 
-                var v = dataArray[i] / 128.0;
-                var y = v * HEIGHT / 2;
-                if (i === 0) {
-                    canvasCtx.moveTo(x, y);
-                } else {
-                    canvasCtx.lineTo(x, y);
-                }
+        x += sliceWidth;
+      }
 
-                x += sliceWidth;
-            }
+      canvasCtx.lineTo(canvas.width, canvas.height / 2);
+      canvasCtx.stroke();
+    };
 
-            canvasCtx.lineTo(canvas.width, canvas.height / 2);
-            canvasCtx.stroke();
-        };
+    draw();
 
-        draw();
+  } else if (visualSetting == "frequencybars") {
+    analyser.fftSize = 256;
+    var bufferLength = analyser.frequencyBinCount;
+    console.log("bufferLength: ", bufferLength);
+    var dataArray = new Uint8Array(bufferLength);
 
-    } else if (visualSetting == "off") {
-        canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
-        canvasCtx.fillStyle = 'red';
-        canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-    }
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    function draw() {
+      drawVisual = requestAnimationFrame(draw);
+
+      analyser.getByteFrequencyData(dataArray);
+
+      canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+      var barWidth = (WIDTH / bufferLength) * 2.5;
+      var barHeight;
+      var x = 0;
+
+      for (var i = 0; i < bufferLength; i++) {
+        // console.log("stream: ", stream);
+        barHeight = dataArray[i];
+
+        canvasCtx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
+        canvasCtx.fillRect(x, HEIGHT-barHeight/2, barWidth, barHeight/2);
+
+        x += barWidth + 1;
+      }
+    };
+
+    draw();
+
+  } else if (visualSetting == "off") {
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+    canvasCtx.fillStyle = 'red';
+    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+  }
 }
 
 function voiceChange() {
   distortion.curve = new Float32Array;
   biquadFilter.gainNode.value = 0;
+  convolver.buffer = undefined;
 
   var voiceSetting = voiceSelect.value;
   console.log(voiceSetting);
@@ -168,7 +219,7 @@ function voiceChange() {
   if (voiceSetting == "distortion") {
     distortion.curve = makeDistortionCurve(400);
   } else if (voiceSetting == "convolver") {
-    convolver.buffer = concertHallSound;
+    convolver.buffer = concertHallBuffer;
   } else if (voiceSetting == "biquad") {
     biquadFilter.type = "lowshelf";
     biquadFilter.frequency.value = 1000;
